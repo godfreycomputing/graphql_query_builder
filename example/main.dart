67ce5bc3fc8457dce35f84880dc45333eae82f4d
godfreycomputing <godfreycomputing@gmail.com>
2021-04-27 07:14:27 -0400

    Removed http dependency and updated example.

--- example/main.dart
+++ example/main.dart
@@ -1,12 +1,9 @@
-import 'dart:convert';
-
-import 'package:http/http.dart' as http;
 import 'package:graphql_query_builder/graphql_query_builder.dart';
 
-Future<List<Category>> searchCategories(
+String buildSearchQuery(
   String searchField,
   String searchTerm,
-) async {
+) {
   final operationName = 'searchCategories';
   final parameters = {'searchField': searchField, 'searchTerm': searchTerm};
   final categoryResultObject = ResultObject({
@@ -21,39 +18,9 @@ Future<List<Category>> searchCategories(
     'message': null
   };
 
-  final queryString = QueryBuilder(
+  return QueryBuilder(
     operationName,
     parameters,
     results,
   ).buildQuery();
-
-  final response = await http.post(
-    Uri(path: 'graphqlUrl'),
-    headers: {'Contexnt-Type': 'application/graphql'},
-    body: queryString,
-  );
-
-  final data = jsonDecode(response.body)['data'][operationName];
-
-  return (data['categories'] as List)
-      .map((categoryData) => Category.fromMap(categoryData))
-      .toList();
-}
-
-class Category {
-  final String? id;
-  final String? name;
-  final String? description;
-  final String? imageUrl;
-
-  Category({this.id, this.name, this.description, this.imageUrl});
-
-  factory Category.fromMap(Map data) {
-    return Category(
-      id: data['id'],
-      name: data['name'],
-      description: data['description'],
-      imageUrl: data['imageUrl'],
-    );
-  }
 }
